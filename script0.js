7b17d9a73727b572a0034af2caa8666aec756405
Sri-kanth-J <146601092+Sri-kanth-J@users.noreply.github.com>
2024-09-19 20:08:06 +0530

    Update script0.js

--- script0.js
+++ script0.js
@@ -1,14 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-    if (window.applicationCache) {
-        window.applicationCache.addEventListener('updateready', function() {
-            if (window.applicationCache.status === window.applicationCache.UPDATEREADY) {
-                // Swap the cache
-                window.applicationCache.swapCache();
-                // Optionally reload the page to use the new cache
-                window.location.reload();
-            }
-        }, false);
+// Check if a new cache is available on page load.
+window.addEventListener('load', function(e) {
+
+  window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      // Browser downloaded a new app cache.
+      // Swap it in and reload the page to get the new hotness.
+      window.applicationCache.swapCache();
+      if (confirm('A new version of this site is available. Load it?')) {
+        window.location.reload();
+      }
+    } else {
+      // Manifest didn't changed. Nothing new to server.
     }
+  }, false);
+
+}, false);
 const semesterSubjects = {
     1: [
         ["HS2121", "Professional English and Functional Skills", 4],
