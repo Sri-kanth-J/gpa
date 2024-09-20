1ced62b33dda86176d47d92c65c28bba00c28cea
Sri-kanth-J <146601092+Sri-kanth-J@users.noreply.github.com>
2024-09-20 21:32:46 +0530

    Update script0.js

--- script0.js
+++ script0.js
@@ -100,12 +100,11 @@ function populateSemester(semester) {
 }
 
 function createTable(subjects, semester) {
-    let tableHTML = '<table><thead><tr><th>S.No.</th><th>Code</th><th>Title</th><th>Credits</th><th>Grade</th></tr></thead><tbody>';
+    let tableHTML = '<table><thead><tr><th>Code</th><th>Title</th><th>Credits</th><th>Grade</th></tr></thead><tbody>';
     
     subjects.forEach((subject, index) => {
         tableHTML += `
             <tr>
-                <td>${index + 1}</td>
                 <td>${subject[0]}</td>
                 <td>${subject[1].length > 20 ? subject[1].substring(0, 20) + '...' : subject[1]}</td>
                 <td>${subject[2]}</td>
